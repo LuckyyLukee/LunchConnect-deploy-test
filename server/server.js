c6d47b5f649be57a2126ec65bf3a23c568234e87
Lucas Magnusson <lucasmyeet@gmail.com>
2024-03-21 13:11:28 +0100

    changed for deployment

--- server/server.js
+++ server/server.js
@@ -29,13 +29,14 @@ app.use("/api", authRouter);
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
+
 app.get("/", (req, res) => {
   res.json("Connected to backend")
 })
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 server.listen(PORT, () => {
   connectDB();
